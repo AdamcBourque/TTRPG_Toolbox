872346c252095f0ea2366eaaec416e2d281367bb
Adam Bourque <46966095+AdamcBourque@users.noreply.github.com>
2020-09-15 13:41:34 -0400

    Updated functions
    Dice roller

--- functions.py
+++ functions.py
@@ -1,11 +1,25 @@
 ## Functions for senior project TTRPG system
 
 from random import *
+from tkinter import *
+from re import *
 
 def diceRoller(diceString):
-    return 0
+    print("Rolling " + diceString + "\n")
+    elems = re.split('[+-]', diceString) 
+    mod = int(elems[-1])
+    totalRoll = mod
+    for j in range (0, len(elems)-1):
+        dice = elems[j].split('d')
+        for i in range (0, int(dice[0])):
+            temp = randint(1, int(dice[1]))
+            totalRoll += temp
+            print(str(temp) + " + ", end = '')
+    print(str(mod) + " = " + str(totalRoll))
+    return totalRoll
 
-def selectFromFile(file):
+## returns one item from a coma seperatied list read from a file
+def selectFromFile(file):  
     text = file.read()
     items = text.split(",")
     return items[randint(0,len(items)-1)]
@@ -30,4 +44,4 @@ fromFile = selectFromFile(file)
 file.close()
 print(fromFile)
 
-roll = diceRoller("2d8+5")
+roll = diceRoller("2d8+4d6+5")
