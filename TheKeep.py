a6137a7144ff191924c2f6a2a80cbdbc8addfaab
Adam Bourque <46966095+AdamcBourque@users.noreply.github.com>
2020-10-22 14:05:00 -0400

    Dice rolleand launch hub with some work on shop

--- TheKeep.py
+++ TheKeep.py
@@ -1,13 +1,49 @@
-# This will import all the widgets 
-# and modules which are available in 
-# tkinter and ttk module 
 from tkinter import *
 from tkinter import font as tkFont
+from re import split      ## for re.split
+from random import *  ## for pseudorandom number generation
 
-expression = ""
+
 BgColor = "grey19"
 lblColor = "grey35"
 ##fontColor = "pink"
+
+## dice roller function takes a string "2d8+5" and prints the rolls and total.
+## returns total as an int
+def diceRoller(diceString):
+    ## print("Rolling " + diceString + "\n")
+    elems = re.split('[+-]', diceString) ##splits into dice types and mod
+
+    if (elems[-1].find('d') == -1): ## Checks if there is a modifier
+        mod = elems[-1] ## stat modifier
+        totalRoll = ""
+        total = int(mod)
+        for j in range (0, len(elems)-1):
+            dice = elems[j].split('d') ## seperates dice quantity and sizes
+            for i in range (0, int(dice[0])):
+                temp = randint(1, int(dice[1])) 
+                total += temp ## adds each dice roll to the total
+                totalRoll += (str(temp) + " + ") ## adds each roll to the return string
+        totalRoll += (str(mod) + " = " + str(total))
+    else:
+        mod = 0
+        totalRoll = ""
+        total = int(mod)
+        for j in range (0, len(elems)):
+            dice = elems[j].split('d') ## seperates dice quantity and sizes
+            for i in range (0, int(dice[0])):
+                temp = randint(1, int(dice[1])) 
+                total += temp ## adds each dice roll to the total
+                totalRoll += (str(temp) + " + ") ## adds each roll to the return string
+        totalRoll += (" = " + str(total))
+    return totalRoll
+
+## returns one item from a coma seperatied list read from a file
+def selectFromFile(file):  
+    text = file.read() 
+    items = text.split(",")
+    items[0] = items[0].replace("ï»¿", '') ## remove garbage from front of string
+    return items[randint(0,len(items)-1)]
 
 ## These classes will be the other tool GUI's
 class NpcGen(Toplevel): 
@@ -16,7 +52,8 @@ class NpcGen(Toplevel):
           
         super().__init__(master = master) 
         self.title("New Window") 
-        self.geometry("200x200") 
+        self.geometry("200x200")
+        
         label = Label(self, text ="This is a new Window")
         label.pack(fill = X, padx=5, pady=5)
 
@@ -26,25 +63,47 @@ class DiceRoller(Toplevel):
     def __init__(self, master = None): 
           
         super().__init__(master = master) 
-        self.title("Newer Window") 
+        self.title("Dice Roller") 
         self.geometry("200x200")
-        global expression
-        expression_field = Entry(self, textvariable=equation) 
-        expression_field.pack(fill = X, padx=5, pady=5)
-        equation.set('enter your expression') 
-        equation.set(expression)
-        label = Label(self, text = equation)
-        label.pack(fill = X, padx=5, pady=5)
+        
+        result = StringVar() ## output
+        dice = StringVar() ## input
+        
+        def writer(*args):  ## roller call
+            result.set(diceRoller(text_entry.get()))
+  
+        text_entry = Entry(self, textvariable=dice)
+        text_entry.pack()
+        
+        button1 = Button(self, text="Roll", command=writer) 
+        button1.pack()
+        
+        label = Label(self, textvariable = result)
+        label.pack()
+        
+        
 
 class ShopInventory(Toplevel): 
       
     def __init__(self, master = None): 
           
         super().__init__(master = master) 
-        self.title("New Window") 
-        self.geometry("200x200") 
+        self.title("Shop Inventory Generator") 
+        self.geometry("200x200")
+        
+        file = open("./TerrainTypes.txt", "r") ## shop types file
+        shops = file.read().split(',') ## shop type list
+        shops[0] = shops[0].replace("ï»¿", '') ## remove garbage from front of string
+        file.close()
+        
+        shopTypes = StringVar()
+        shopTypes.set(shops[0])
+
+        opt = OptionMenu(self, shopTypes, *shops) ## drop down menu select
+        opt.pack()
+        
         label = Label(self, text ="This is a new Window")
-        label.pack(fill = X, padx=5, pady=5)
+        label.pack()
 
 class StatusTracker(Toplevel): 
       
@@ -52,7 +111,8 @@ class StatusTracker(Toplevel):
           
         super().__init__(master = master) 
         self.title("New Window") 
-        self.geometry("200x200") 
+        self.geometry("200x200")
+        
         label = Label(self, text ="This is a new Window")
         label.pack(fill = X, padx=5, pady=5)
 
@@ -62,7 +122,8 @@ class BattlemapGen(Toplevel):
           
         super().__init__(master = master) 
         self.title("New Window") 
-        self.geometry("200x200") 
+        self.geometry("200x200")
+        
         label = Label(self, text ="This is a new Window")
         label.pack(fill = X, padx=5, pady=5)
 
@@ -72,7 +133,8 @@ class MusicManage(Toplevel):
           
         super().__init__(master = master) 
         self.title("New Window") 
-        self.geometry("200x200") 
+        self.geometry("200x200")
+        
         label = Label(self, text ="This is a new Window")
         label.pack(fill = X, padx=5, pady=5)
 
@@ -82,7 +144,8 @@ class OverlandManage(Toplevel):
           
         super().__init__(master = master) 
         self.title("New Window") 
-        self.geometry("200x200") 
+        self.geometry("200x200")
+        
         label = Label(self, text ="This is a new Window")
         label.pack(fill = X, padx=5, pady=5)
 
@@ -92,7 +155,8 @@ class CharacterManage(Toplevel):
           
         super().__init__(master = master) 
         self.title("New Window") 
-        self.geometry("200x200") 
+        self.geometry("200x200")
+        
         label = Label(self, text ="This is a new Window")
         label.pack(fill = X, padx=5, pady=5)
 
@@ -102,7 +166,8 @@ class EncounterGen(Toplevel):
           
         super().__init__(master = master) 
         self.title("New Window") 
-        self.geometry("200x200") 
+        self.geometry("200x200")
+        
         label = Label(self, text ="This is a new Window")
         label.pack(fill = X, padx=5, pady=5)
 
