5d1f364c8dc79317b1c9c09dc2c54694f0ae303a
Adam Bourque <46966095+AdamcBourque@users.noreply.github.com>
2020-10-29 23:42:02 -0400

    shop inventory and npc generator

--- TheKeep.py
+++ TheKeep.py
@@ -98,12 +98,26 @@ def diceRoller(diceString):
                
 
 def readCSV(filename):  ## makes a list from a csv
-    file = open(filename, "r")
+    file = open(filename, "r", encoding="utf8")
     text = file.read()
     file.close()
     items = text.split(",")
     items[0] = items[0].replace("ï»¿", '') ## remove garbage from front of string
     return items
+
+def readTupleCSV(filename):
+    file = open(filename, "r", encoding="utf8")
+    text = file.read()
+    file.close()
+    items = text.split("),")
+    items[0] = items[0].replace("ï»¿", '') ## remove garbage from front of string
+    tuples = []
+    for i in items:
+        i = i.replace("(", '')
+        i = i.replace(")", '')
+        temp = i.split(',')
+        tuples.append(temp)
+    return tuples
 
 ## returns one item from a coma seperatied list
 def selectFromList(items):  
@@ -119,11 +133,30 @@ class NpcGen(Toplevel):
     def __init__(self, master = None): 
           
         super().__init__(master = master) 
-        self.title("New Window") 
-        self.geometry("200x200")
+        self.title("NPC Generator") 
+        self.geometry("400x400")
+
+        types = readCSV("./ShopTypes.txt") ## list of NPC types
         
-        label = Label(self, text ="This is a new Window")
-        label.pack(fill = X, padx=5, pady=5)
+        Types = StringVar()  ## currently selected NPC type
+        Types.set(types[0])  
+
+        opt = OptionMenu(self, Types, *types) ## drop down menu select
+        opt.pack()
+
+        def NPC_Gen(*args):
+            output = StringVar()
+            temp = ""
+            NPC_Type = ("./" + Types.get() + ".txt") ## file path of inventory csv for selected type
+            npcs = readCSV(NPC_Type) ## list of npc possibilities 
+
+            output.set(temp)
+            label = Label(self, textvariable = output) ## output label
+            label.pack()
+            
+        button1 = Button(self, text="Generate", command=NPC_Gen) ## button to run process 
+        button1.pack()
+
 
 
 class DiceRoller(Toplevel): 
@@ -140,13 +173,13 @@ class DiceRoller(Toplevel):
         def writer(*args):  ## roller call
             result.set(diceRoller(text_entry.get()))
   
-        text_entry = Entry(self, textvariable=dice)
+        text_entry = Entry(self, textvariable=dice) ## field for dice entry
         text_entry.pack()
         
-        button1 = Button(self, text="Roll", command=writer) 
+        button1 = Button(self, text="Roll", command=writer) ## button to roll
         button1.pack()
         
-        label = Label(self, textvariable = result)
+        label = Label(self, textvariable = result) ## results label
         label.pack()
         
         
@@ -158,9 +191,6 @@ class ShopInventory(Toplevel):
         super().__init__(master = master) 
         self.title("Shop Inventory Generator") 
         self.geometry("400x400")
-
-        rarity = {'c':(10,30), 'u':(5,10), 'r':(1,5), 'v':(0,1)} ## rarity to quantity dictionary 
-        output = StringVar()
         
         shops = readCSV("./ShopTypes.txt") ## list of shop types
         
@@ -169,15 +199,23 @@ class ShopInventory(Toplevel):
 
         opt = OptionMenu(self, shopTypes, *shops) ## drop down menu select
         opt.pack()
-        
-        shop = ("./" + shopTypes + ".txt") ## file path of inventory csv for selected shop
-        items = readCSV(shop) ## list of Items
 
-        for i in items:
-            output += (i[0] + ": " + str(randint(rarity[i[1]][0],rarity[i[1]][1])) + "\n")
+        def GenInventory(*args):
+            shop = ("./" + shopTypes.get() + "Inventory.txt") ## file path of inventory csv for selected shop
+            items = readTupleCSV(shop) ## list of Items
+            rarity = {'C':(10,30), 'U':(5,10), 'R':(1,5), 'V':(0,1)} ## rarity to quantity dictionary
+            temp = ""
+            output = StringVar()
 
-        label = Label(self, textvariable = output)
-        label.pack()
+            for i in items:
+                temp += (i[0] + ": " + str(randint(rarity[i[1]][0],rarity[i[1]][1])) + " - " + i[2] + "\n")
+
+            output.set(temp)
+            label = Label(self, textvariable = output)
+            label.pack()
+            
+        button1 = Button(self, text="Generate", command=GenInventory) 
+        button1.pack()
 
 class StatusTracker(Toplevel): 
       
