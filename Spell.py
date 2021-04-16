0877c6092ad26029ff969e3310bd2d98f4e85ee8
Adam Bourque <46966095+AdamcBourque@users.noreply.github.com>
2021-04-16 10:41:10 -0400

    Add files via upload

--- Spell.py
+++ Spell.py
@@ -9,10 +9,26 @@ class Spell:
 
   name = ""
   lvl = 0
-  spell_type = ""
+  school = ""
+  action = ""
+  components = (0,0,"")
+  duration = ""
+  spell_range = 0
+  ritual = 0
+  description = ""
   
   
-  def __init__(self, *spell):
+  def __init__(self, spell):
     self.name = spell[0]
     self.lvl = spell[1]
-    self.spell_type = spell[2]
+    self.school = spell[2]
+    self.action = spell[3]
+   # self.components = spell[4] + 
+    self.duration = spell[7]
+    self.spell_range = spell[8]
+    self.ritual = spell[9]
+    self.description = spell[10]
+
+
+  def display():
+    print("Not Implimented")
